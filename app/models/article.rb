4e1a4ec2849d4e1e697abd3c1a9a871702b44a61
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-07-17 17:11:04 +0200

    ajout de la recherche

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
   has_one_attached :photo
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_body,
+    against: [ :title, :body ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
